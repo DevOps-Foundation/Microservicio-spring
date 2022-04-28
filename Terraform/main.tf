7a5a06a56b8ec62a61248fb0bf60109e93bf8b7d
clagosu <clagosu@users.noreply.github.com>
2022-04-27 22:11:59 -0400

    Se agrega Terraform

---
+++ Terraform/main.tf
@@ -0,0 +1,28 @@
+
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "=2.5.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id = var.subscription_id
+  client_id       = var.serviceprinciple_id
+  client_secret   = var.serviceprinciple_key
+  tenant_id       = var.tenant_id
+
+  features {}
+}
+
+module "cluster" {
+  source                = "./modules/cluster/"
+  serviceprinciple_id   = var.serviceprinciple_id
+  serviceprinciple_key  = var.serviceprinciple_key
+  ssh_key               = var.ssh_key
+  location              = var.location
+  kubernetes_version    = var.kubernetes_version  
+  
+}
---
+++ Terraform/modules/cluster/variables.tf
@@ -0,0 +1,16 @@
+variable "serviceprinciple_id" {
+}
+
+variable "serviceprinciple_key" {
+}
+
+variable "location" {
+  default = "brazilsouth"
+}
+
+variable "kubernetes_version" {
+    default = "1.22.6"
+}
+
+variable "ssh_key" {
+}
